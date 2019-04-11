bd89403170d328c766361cf011ac324df05e17ea
Susan Tan <ssusantan9@gmail.com>
2019-04-11 21:05:01 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
  class Artist < ActiveRecord::Base
+=======
+ 
+class Artist < ActiveRecord::Base
+>>>>>>> c93625574491de0728f65390095b79c844c614a2
 end
--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,10 +1,13 @@
 class CreateArtists < ActiveRecord::Migration
+<<<<<<< HEAD
   def up
   end
  
   def down
   end
   
+=======
+>>>>>>> c93625574491de0728f65390095b79c844c614a2
   def change
     create_table :artists do |t|
       t.string :name
@@ -13,5 +16,9 @@ class CreateArtists < ActiveRecord::Migration
       t.string :hometown
     end
   end
+<<<<<<< HEAD
 end
 
+=======
+end
+>>>>>>> c93625574491de0728f65390095b79c844c614a2
